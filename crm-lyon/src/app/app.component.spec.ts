cc655b11e374d597ecf38b906fc33048b2c2082e
Eleyone <eleyone@gmail.com>
2018-09-10 11:51:55 +0200

    Init Angular App

--- crm-lyon/src/app/app.component.spec.ts
+++ crm-lyon/src/app/app.component.spec.ts
@@ -13,15 +13,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'formation-app'`, async(() => {
+  it(`should have as title 'crm-lyon'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('formation-app');
+    expect(app.title).toEqual('crm-lyon');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to formation-app!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to crm-lyon!');
   }));
 });
